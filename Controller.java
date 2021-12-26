d4253ce4ef3b2463cf994d42b51928e211abade9
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2021-12-26 23:38:15 +0200

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,13 @@
+package Phone_Book;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void add(ActionEvent actionEvent) {
+        System.out.println("Add pressed");
+    }
+
+    public void delete(ActionEvent actionEvent) {
+        System.out.println("Delete pressed");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,23 @@
+package Phone_Book;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        primaryStage.setTitle("My Phone Book");
+        primaryStage.setScene(new Scene(root, 300, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
